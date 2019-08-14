bd7d234a6b05cc05f31b7ae1d5515cabc3399342
plutobell <wiaoxiaoshuo@163.com>
2019-08-15 00:14:30 +0800

    add urlencode

--- plugins/Chat/Chat.py
+++ plugins/Chat/Chat.py
@@ -1,14 +1,14 @@
 import sys,requests
 sys.path.append("../")
 from TeeleBot import teelebot
+import urllib.parse as ubp
 
 def Chat(fromm, chat, date, text):
     url = "http://api.qingyunke.com/api.php?key=free&appid=0&msg="
     url1 = "https://api.ownthink.com/bot?spoken="
     requests.adapters.DEFAULT_RETRIES = 5
-    req = requests.get(url + text[1:])
+    req = requests.get(url + ubp.quote(text[1:])) #urlencode编码
     req.keep_alive = False
     req.encoding = "utf-8"
-
     bot = teelebot.Bot()
     status = bot.sendMessage(chat["id"], str(req.json().get("content").replace("{br}", "%0A")), "html")
