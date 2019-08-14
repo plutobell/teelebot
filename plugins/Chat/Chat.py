88b70583f97e47d6cd8af22592385b48f300bb85
plutobell <53148519+plutobell@users.noreply.github.com>
2019-08-14 23:02:53 +0800

    Replace the Chat API to qingyunke

--- plugins/Chat/Chat.py
+++ plugins/Chat/Chat.py
@@ -3,16 +3,12 @@ sys.path.append("../")
 from TeeleBot import teelebot
 
 def Chat(fromm, chat, date, text):
-    url1 = "http://api.qingyunke.com/api.php?key=free&appid=0&msg="
-    url = "https://api.ownthink.com/bot?spoken="
+    url = "http://api.qingyunke.com/api.php?key=free&appid=0&msg="
+    url1 = "https://api.ownthink.com/bot?spoken="
     requests.adapters.DEFAULT_RETRIES = 5
-    status = True
-    while(status):
-        req = requests.get(url + text[1:])
-        req.keep_alive = False
-        req.encoding = "utf-8"
-        if req.json().get("message") == "success":
-            status = False
+    req = requests.get(url + text[1:])
+    req.keep_alive = False
+    req.encoding = "utf-8"
 
     bot = teelebot.Bot()
-    status = bot.sendMessage(chat["id"], str(req.json().get("data").get("info")["text"]), "text")
+    status = bot.sendMessage(chat["id"], str(req.json().get("content"), "html")
