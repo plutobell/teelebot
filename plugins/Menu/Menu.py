fbda6c9d9ed372ee10f7170a4454dcf7b1f1d8ce
plutobell <wiaoxiaoshuo@163.com>
2019-08-15 23:59:49 +0800

    change the command /menu to /start

--- plugins/Menu/Menu.py
+++ plugins/Menu/Menu.py
@@ -20,6 +20,6 @@ def Menu(message):
                 elif i == 1:
                     line_2 = f.readline().strip()[1:]
             menu_str += line_1 + " - " + line_2 + "%0A%0A"
-    menu_str = "===== Command Menu =====%0A%0A" + menu_str
+    menu_str = "===== Command Menu =====%0A%0A" + menu_str + "%0A%0Av1.0.7"
     bot = teelebot.Bot()
     status = bot.sendMessage(message["chat"]["id"], menu_str, "html")
