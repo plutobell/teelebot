31747d92c803466c707c9910da13206d91be1633
plutobell <wiaoxiaoshuo@163.com>
2019-08-18 13:51:54 +0800

    set default coding

--- plugins/Chat/Chat.py
+++ plugins/Chat/Chat.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import sys,requests
 sys.path.append("../")
 from TeeleBot import teelebot
--- plugins/Menu/Menu.py
+++ plugins/Menu/Menu.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import sys, os
 sys.path.append("../")
 from TeeleBot import teelebot
