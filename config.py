fb52d75471adac737c32f6c23023fa47ae7f42d6
plutobell <wiaoxiaoshuo@163.com>
2019-08-22 17:04:53 +0800

    Perfect config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
-import configparser
+import configparser, sys
+
+__version__ = "1.1.2"
+__author__ = "github:plutobell"
 
 config = {}
 
@@ -10,7 +13,11 @@ options = conf.options("config")
 for option in options:
 	config[str(option)] = conf.get("config", option)
 
+if any([ "version" in config.keys(), "author" in config.keys() ]):
+    print("配置文件存在错误!")
+    sys.exit(0)
 
-config["version"] = '1.1.2'
+config["author"] = __author__
+config["version"] = __version__
 
 #print(config)
