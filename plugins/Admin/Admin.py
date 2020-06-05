e40a750d52bb59da7be27c5f120c50784048839d
plutobell <53148519+plutobell@users.noreply.github.com>
2020-06-05 09:38:42 +0800

    Improve plugin Admin

--- plugins/Admin/Admin.py
+++ plugins/Admin/Admin.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 '''
 creation time: 2020-6-4
-last_modify: 2020-6-4
+last_modify: 2020-6-5
 '''
 
 from teelebot import Bot
@@ -20,9 +20,13 @@ def Admin(message):
     bot_id = str(bot.getMe()["id"])
     prefix = "admin"
 
-    commad = {
+    commad = { #命令注册
             "/adminkick": "kick",
-            "/admindel": "del"
+            "/admindel": "del",
+            "/adminpin": "pin",
+            "/adminunpin": "unpin",
+            "/adminmute": "mute",
+            "/adminunmute": "unmute"
         }
     count = 0
     for c in commad.keys():
@@ -44,7 +48,11 @@ def Admin(message):
         status = bot.sendChatAction(chat_id, "typing")
         msg = "<b>===== Admin 插件功能 =====</b>%0A%0A" +\
             "<b>/adminkick</b> - 踢人。格式:以回复要踢用户的消息的形式发送指令%0A" +\
-            "<b>/admindel</b> - 删除一条消息。格式:以回复要删除的消息的形式发送指令%0A" +\
+            "<b>/admindel</b> - 删除消息。格式:以回复要删除的消息的形式发送指令%0A" +\
+            "<b>/adminpin</b> - 置顶消息。格式:以回复要置顶的消息的形式发送指令%0A" +\
+            "<b>/adminunpin</b> - 取消置顶。格式:以回复要取消置顶的消息的形式发送指令%0A" +\
+            "<b>/adminmute</b> - 禁言用户。格式:以回复要禁言用户的消息的形式发送指令，指令后跟禁言时间(支持的时间：1m,10m,1h,1d,forever)，以空格作为分隔符%0A" +\
+            "<b>/adminunmute</b> - 解除用户禁言。格式:以回复要解除禁言用户的消息的形式发送指令%0A" +\
             "%0A..."
         status = bot.sendMessage(chat_id=chat_id, text=msg, parse_mode="HTML", reply_to_message_id=message["message_id"])
 
@@ -74,6 +82,85 @@ def Admin(message):
                     timer.start()
             elif text[1:] == prefix + commad["/admindel"]:
                 status = bot.deleteMessage(chat_id=chat_id, message_id=target_message_id)
+                if status == False:
+                    status = bot.sendChatAction(chat_id, "typing")
+                    status = bot.sendMessage(chat_id=chat_id, text="删除失败!", parse_mode="text", reply_to_message_id=message["message_id"])
+                    timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                    timer.start()
+            elif text[1:] == prefix + commad["/adminpin"]:
+                status = bot.pinChatMessage(chat_id=chat_id, message_id=target_message_id)
+                if status == False:
+                    status = bot.sendChatAction(chat_id, "typing")
+                    status = bot.sendMessage(chat_id=chat_id, text="置顶失败!", parse_mode="text", reply_to_message_id=message["message_id"])
+                    timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                    timer.start()
+            elif text[1:] == prefix + commad["/adminunpin"]:
+                status = bot.unpinChatMessage(chat_id=chat_id)
+                if status == False:
+                    status = bot.sendChatAction(chat_id, "typing")
+                    status = bot.sendMessage(chat_id=chat_id, text="取消置顶失败!", parse_mode="text", reply_to_message_id=message["message_id"])
+                    timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                    timer.start()
+            elif text[1:len(prefix + commad["/adminmute"])+1] == prefix + commad["/adminmute"]:
+                if str(target_user_id) not in admins:
+                    mute_time = {
+                        "1m": 1 * 60,
+                        "10m": 1 * 60 * 10,
+                        "1h": 1 * 60 * 60,
+                        "1d": 1 * 60 * 60 * 24,
+                        "forever": 0
+                    }
+                    permissions = {
+                        'can_send_messages':False,
+                        'can_send_media_messages':False,
+                        'can_send_polls':False,
+                        'can_send_other_messages':False,
+                        'can_add_web_page_previews':False,
+                        'can_change_info':False,
+                        'can_invite_users':False,
+                        'can_pin_messages':False
+                    }
+                    if text[1:].split(' ')[1] in mute_time.keys():
+                        status = bot.restrictChatMember(chat_id=chat_id, user_id=target_user_id,permissions=permissions, until_date=mute_time[text[1:].split(' ')[1]])
+                        if status != False:
+                            status = bot.sendChatAction(chat_id, "typing")
+                            msg = "<b><a href='tg://user?id=" + str(target_user_id) + "'>" + str(target_user_id) + "</a></b> 已被禁言，持续时间：<b>" + str(mute_time[text[1:].split(' ')[1]]) + "</b>。"
+                            status = bot.sendMessage(chat_id=chat_id, text=msg, parse_mode="HTML", reply_to_message_id=message["message_id"])
+                            timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                            timer.start()
+                        else:
+                            status = bot.sendChatAction(chat_id, "typing")
+                            msg = "<b><a href='tg://user?id=" + str(target_user_id) + "'>" + str(target_user_id) + "</a></b> 禁言失败!"
+                            status = bot.sendMessage(chat_id=chat_id, text=msg, parse_mode="HTML", reply_to_message_id=message["message_id"])
+                            timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                            timer.start()
+                    else:
+                        status = bot.sendChatAction(chat_id, "typing")
+                        status = bot.sendMessage(chat_id=chat_id, text="无效指令，请检查格式!", parse_mode="text", reply_to_message_id=message["message_id"])
+                        timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                        timer.start()
+                else:
+                    status = bot.sendChatAction(chat_id, "typing")
+                    status = bot.sendMessage(chat_id=chat_id, text="抱歉，无权处置该用户!", parse_mode="text", reply_to_message_id=message["message_id"])
+                    timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                    timer.start()
+            elif text[1:] == prefix + commad["/adminunmute"]:
+                if str(target_user_id) not in admins:
+                    status = bot.getChat(chat_id=chat_id)
+                    permissions = status.get("permissions")
+                    status = bot.restrictChatMember(chat_id=chat_id, user_id=target_user_id,permissions=permissions)
+                    if status != False:
+                        status = bot.sendChatAction(chat_id, "typing")
+                        msg = "<b><a href='tg://user?id=" + str(target_user_id) + "'>" + str(target_user_id) + "</a></b> 已被解禁。"
+                        status = bot.sendMessage(chat_id=chat_id, text=msg, parse_mode="HTML", reply_to_message_id=message["message_id"])
+                        timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                        timer.start()
+                else:
+                    status = bot.sendChatAction(chat_id, "typing")
+                    status = bot.sendMessage(chat_id=chat_id, text="抱歉，无权处置该用户!", parse_mode="text", reply_to_message_id=message["message_id"])
+                    timer = Timer(gap, timer_func_for_del, args=[chat_id, status["message_id"]])
+                    timer.start()
+
 
         else:
             status = bot.sendChatAction(chat_id, "typing")
