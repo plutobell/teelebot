333cc268b9f3d09ca898b5e00564f6b16f8fa000
plutobell <53148519+plutobell@users.noreply.github.com>
2020-06-10 03:05:54 +0800

    v1.4.7_dev

--- teelebot/teelebot.py
+++ teelebot/teelebot.py
@@ -2,9 +2,9 @@
 '''
 @description:基于Telegram Bot Api 的机器人
 @creation date: 2019-8-13
-@last modify: 2020-6-8
+@last modify: 2020-6-10
 @author github:plutobell
-@version: 1.4.6_dev
+@version: 1.4.7_dev
 '''
 import time
 import sys
