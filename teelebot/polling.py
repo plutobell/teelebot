5940159239bf09391b05b281f8f35b2c1e042ac0
plutobell <53148519+plutobell@users.noreply.github.com>
2020-06-26 14:05:36 +0800

    Improve the response speed of Polling mode

--- teelebot/polling.py
+++ teelebot/polling.py
@@ -1,13 +1,12 @@
 # -*- coding:utf-8 -*-
 '''
 @creation date: 2020-6-23
-@last modify: 2020-6-23
+@last modify: 2020-6-26
 '''
 import time
 import sys
 
 def runUpdates(bot):
-        #print("debug=" + str(self.debug))
         plugin_list = bot.plugin_bridge.keys()
         while True:
             try:
@@ -17,6 +16,5 @@ def runUpdates(bot):
                     continue
                 for message in messages: #获取单条消息记录message
                     bot._pluginRun(bot, message)
-                time.sleep(0.2) #经测试，延时0.2s较为合理
-            except KeyboardInterrupt: #判断键盘输入，终止循环
+            except KeyboardInterrupt:
                 sys.exit("程序终止") #退出存在问题，待修复
