68a3eba63ba83b5cb148188d8ec5a20aebee9d47
plutobell <53148519+plutobell@users.noreply.github.com>
2020-06-26 14:14:54 +0800

    Add new plugin Uptime

---
+++ teelebot/plugins/Uptime/Uptime.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+'''
+creation time: 2020-6-26
+last_modify: 2020-6-26
+'''
+
+def Uptime(bot, message):
+
+    chat_id = message["chat"]["id"]
+    message_id = message["message_id"]
+    text = message["text"]
+    prefix = "uptime"
+
+    if text[1:len(prefix)+1] == prefix:
+        time_format = bot.uptime(time_format="format")
+        time_second = bot.uptime(time_format="second")
+        status = bot.sendChatAction(chat_id, "typing")
+        msg = "感谢您的关心 <b>(￣ε ￣)</b> %0A%0A我已经稳定运行 <b>" +  str(time_second) + "</b> 秒%0A" +\
+                "%0A也就是：<b>" + str(time_format) + "</b>%0A%0A"
+        status = bot.sendMessage(chat_id=chat_id, text=msg, parse_mode="HTML", reply_to_message_id=message_id)
+        bot.message_deletor(15, chat_id, status["message_id"])
