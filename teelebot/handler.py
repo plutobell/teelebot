9527f467c512785f083947f9dbb61537358e5575
plutobell <53148519+plutobell@users.noreply.github.com>
2020-06-29 00:40:20 +0800

    Enhanced command line commands

--- teelebot/handler.py
+++ teelebot/handler.py
@@ -1,36 +1,51 @@
 # -*- coding:utf-8 -*-
 '''
 @creation date: 2019-8-23
-@last modify: 2020-6-27
+@last modify: 2020-6-29
 '''
 import configparser
+import argparse
 import os
 import sys
+import shutil
+
+__version__ = "1.8.8_dev"
+__author__ = "github:plutobell"
+
+parser = argparse.ArgumentParser(description="teelebot console command list")
+parser.add_argument("-c", "--config", type=str, help="specify the configuration file path")
+parser.add_argument("-d", "--debug", help="run teelebot in debug mode", action="store_true")
+parser.add_argument("-v", "--version", help="view the current version of teelebot", action="store_true")
+args = parser.parse_args()
+
+if len(sys.argv) == 2 and args.version:
+    os.system("")
+    print("version: \033[1;36;40m" + __version__ + "\033[0m")
+    print("author: \033[1;36;40m" + __author__ + "\033[0m")
+    print("")
+    sys.exit(0)
 
 def config():
-    __version__ = "1.8.6_dev"
-    __author__ = "github:plutobell"
-
     config = {}
 
-    if len(sys.argv) == 3 and sys.argv[1] in ("-c", "-C"):
-        config_dir = os.path.abspath(sys.argv[2])
-        #print(config_dir)
-    elif len(sys.argv) == 1 or  len(sys.argv) == 2 and sys.argv[1] in ("check", "sdist", "bdist_wheel", "bdist_rpm"):
+    if len(sys.argv) == 3 and args.config:
+        config_dir = os.path.abspath(args.config)
+    elif len(sys.argv) == 1 or args.debug or len(sys.argv) == 2 and sys.argv[1] in ("check", "sdist", "bdist_wheel", "bdist_rpm"):
         if not os.path.exists(os.path.abspath(os.path.expanduser('~')) + "/.teelebot"):
             os.mkdir(os.path.abspath(os.path.expanduser('~')) + "/.teelebot")
         config_dir = os.path.abspath(os.path.expanduser('~')) + "/.teelebot/config.cfg"
         #config_dir = os.path.dirname(os.path.abspath(__file__)) + "/config.cfg"
-        #print(config_dir)
     else:
-        print("参数缺失或错误!")
-        sys.exit(0)
+        sys.exit("参数缺失或错误!")
 
     conf = configparser.ConfigParser()
     conf.read(config_dir)
     options = conf.options("config")
 
-    default_args = ["key", "webhook", "debug", "root", "timeout"]
+    if args.debug:
+        default_args = ["key", "webhook", "debug", "root", "timeout"]
+    else:
+        default_args = ["key", "webhook", "root", "timeout"]
     for default_arg in default_args:
         if default_arg not in options:
             print("配置文件缺失必要的参数!")
@@ -54,7 +69,11 @@ def config():
     if "plugin_dir" in config.keys():
         plugin_dir = os.path.abspath(config["plugin_dir"]) + r'/'
     else:
-        plugin_dir = os.path.dirname(os.path.abspath(__file__)) + "/plugins/"
+        plugin_dir = os.path.dirname(os.path.abspath(__file__)) + r"/plugins/"
+
+    if os.path.exists(os.path.dirname(os.path.abspath(__file__)) + r"/__pycache__"):
+        shutil.rmtree(os.path.dirname(os.path.abspath(__file__)) + r"/__pycache__")
+
 
     if not os.path.isdir(plugin_dir): #插件目录检测
         #os.makedirs(plugin_dir)
@@ -88,6 +107,9 @@ def config():
     config["plugin_bridge"] = bridge(config["plugin_dir"])
     config["plugin_info"] = __plugin_info(config["plugin_bridge"].values(), config["plugin_dir"])
 
+    if args.debug:
+        config["debug"] = True
+
     #print(config)
     return config
 
@@ -97,7 +119,7 @@ def bridge(plugin_dir):
 
     plugin_lis = os.listdir(plugin_dir)
     for plugi in plugin_lis:
-        if os.path.isdir(plugin_dir + plugi) and plugi != "__pycache__":
+        if os.path.isdir(plugin_dir + plugi) and plugi != "__pycache__" and plugi[0] != '.':
             plugin_list.append(plugi)
     for plugin in plugin_list:
         with open(plugin_dir + plugin + r"/__init__.py", encoding="utf-8") as f:
@@ -115,6 +137,7 @@ def __plugin_info(plugin_list, plugin_dir):
         plugin_info[plugin] = mtime
 
     return plugin_info
+
 
 
 
