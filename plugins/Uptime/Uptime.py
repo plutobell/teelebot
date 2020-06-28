3a7cc8ce6c1ba48bbcf68e2c2be555d16768f156
plutobell <53148519+plutobell@users.noreply.github.com>
2020-06-29 00:43:08 +0800

    Added display command response times

--- plugins/Uptime/Uptime.py
+++ plugins/Uptime/Uptime.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 '''
 creation time: 2020-6-26
-last_modify: 2020-6-26
+last_modify: 2020-6-29
 '''
 
 def Uptime(bot, message):
@@ -14,8 +14,10 @@ def Uptime(bot, message):
     if text[1:len(prefix)+1] == prefix:
         time_format = bot.uptime(time_format="format")
         time_second = bot.uptime(time_format="second")
+        response_times = bot.response_times()
         status = bot.sendChatAction(chat_id, "typing")
         msg = "感谢您的关心 <b>(￣ε ￣)</b> %0A%0A我已经运行 <b>" +  str(time_second) + "</b> 秒%0A" +\
-                "%0A即：<b>" + str(time_format) + "</b>%0A%0A"
+                "即：<b>" + str(time_format) + "</b>%0A%0A" +\
+                "在此期间累计响应指令 <b>" + str(response_times) + "</b> 次%0A%0A"
         status = bot.sendMessage(chat_id=chat_id, text=msg, parse_mode="HTML", reply_to_message_id=message_id)
         bot.message_deletor(15, chat_id, status["message_id"])
