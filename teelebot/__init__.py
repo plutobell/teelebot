41e4a3005bef30a1bb3bdda933c38c538681d499
plutobell <53148519+plutobell@users.noreply.github.com>
2020-07-11 11:11:40 +0800

    Code optimization

--- teelebot/__init__.py
+++ teelebot/__init__.py
@@ -1,33 +1,38 @@
 # -*- coding:utf-8 -*-
 '''
 @creation date: 2019-8-23
-@last modify: 2020-6-25
+@last modify: 2020-7-11
 '''
+from .polling import runUpdates
+from .webhook import runWebhook
+from .teelebot import Bot
 name = "teelebot"
 __all__ = ['Bot']
 
-from .teelebot import Bot
-from .webhook import runWebhook
-from .polling import runUpdates
 
 bot = Bot()
+
 
 def main():
     if bot.config["webhook"] == True:
         print(" * 正在自检", end="\r")
         status = bot.getWebhookInfo()
-        url = "https://" + str(bot.config["server_address"] + ":" +str(bot.config["server_port"]) + "/bot" + str(bot.config["key"]))
+        url = "https://" + str(bot.config["server_address"] + ":" + str(
+            bot.config["server_port"]) + "/bot" + str(bot.config["key"]))
         if status != False:
             if status["url"] != url or status["has_custom_certificate"] == False or status["max_connections"] != int(bot.config["pool_size"]):
-                status = bot.setWebhook(url=url, certificate=bot.config["cert_pub"], max_connections=bot.config["pool_size"])
+                status = bot.setWebhook(
+                    url=url, certificate=bot.config["cert_pub"], max_connections=bot.config["pool_size"])
                 if status != True:
                     print("设置Webhook失败!")
                     return False
         else:
             print("获取运行模式失败!")
 
-        print(" * 机器人开始运行", "\n * 框架版本：teelebot v" + bot.VERSION, "\n * 运行模式: Webhook", "\n * 最大线程: " + str(bot.config["pool_size"]))
-        runWebhook(bot=bot, host=bot.config["local_address"], port=int(bot.config["local_port"]))
+        print(" * 机器人开始运行", "\n * 框架版本：teelebot v" + bot.VERSION,
+              "\n * 运行模式: Webhook", "\n * 最大线程: " + str(bot.config["pool_size"]))
+        runWebhook(bot=bot, host=bot.config["local_address"], port=int(
+            bot.config["local_port"]))
     else:
         print(" * 正在自检", end="\r")
         status = bot.getWebhookInfo()
@@ -40,5 +45,6 @@ def main():
         else:
             print("获取运行模式失败!")
 
-        print(" * 机器人开始运行", "\n * 框架版本：teelebot v" + bot.VERSION, "\n * 运行模式: Polling", "\n * 最大线程: " + str(bot.config["pool_size"]))
+        print(" * 机器人开始运行", "\n * 框架版本：teelebot v" + bot.VERSION,
+              "\n * 运行模式: Polling", "\n * 最大线程: " + str(bot.config["pool_size"]))
         runUpdates(bot=bot)
--- teelebot/polling.py
+++ teelebot/polling.py
@@ -1,20 +1,21 @@
 # -*- coding:utf-8 -*-
 '''
 @creation date: 2020-6-23
-@last modify: 2020-6-26
+@last modify: 2020-7-11
 '''
 import time
 import sys
 
+
 def runUpdates(bot):
-        plugin_list = bot.plugin_bridge.keys()
-        while True:
-            try:
-                results = bot.getUpdates() #获取消息队列messages
-                messages = bot._washUpdates(results)
-                if messages == None or messages == False:
-                    continue
-                for message in messages: #获取单条消息记录message
-                    bot._pluginRun(bot, message)
-            except KeyboardInterrupt:
-                sys.exit("程序终止") #退出存在问题，待修复
+    plugin_list = bot.plugin_bridge.keys()
+    while True:
+        try:
+            results = bot.getUpdates()  # 获取消息队列messages
+            messages = bot._washUpdates(results)
+            if messages == None or messages == False:
+                continue
+            for message in messages:  # 获取单条消息记录message
+                bot._pluginRun(bot, message)
+        except KeyboardInterrupt:
+            sys.exit("程序终止")  # 退出存在问题，待修复
