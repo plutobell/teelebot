a818c943bfef31691890857d3041d7dc0da8010a
plutobell <53148519+plutobell@users.noreply.github.com>
2020-07-23 22:49:05 +0800

    Bug fix for plugin Sticker

--- plugins/Sticker/Sticker.py
+++ plugins/Sticker/Sticker.py
@@ -12,6 +12,13 @@ def Sticker(bot, message):
             if "sticker" in message["reply_to_message"].keys():
                 file_id = message["reply_to_message"]["sticker"]["file_id"]
                 file_dl_path = bot.getFileDownloadPath(file_id=file_id)
+
+                if ".tgs" in file_dl_path:
+                    status = bot.sendChatAction(chat_id, "typing")
+                    status = bot.sendMessage(chat_id=chat_id, text="暂不支持获取tgs格式的贴纸!", parse_mode="text", reply_to_message_id=message_id)
+                    bot.message_deletor(15, chat_id, status["message_id"])
+                    return
+
                 req = requests.get(url=file_dl_path)
                 if type(req.content) == bytes:
                     photo = req.content
@@ -19,6 +26,7 @@ def Sticker(bot, message):
                     photo = file_dl_path
                 status = bot.sendChatAction(chat_id, "typing")
                 status = bot.sendPhoto(chat_id=chat_id, photo=photo, caption="本消息不久将被销毁，请尽快保存。" , reply_to_message_id=message_id)
+                print(status)
                 bot.message_deletor(30, chat_id, status["message_id"])
             else:
                 status = bot.sendChatAction(chat_id, "typing")
