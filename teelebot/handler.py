693441a710278a843c86ce5a282f6a4cb0b97999
plutobell <53148519+plutobell@users.noreply.github.com>
2020-10-30 00:32:20 +0800

    v1.9.16_dev

--- teelebot/handler.py
+++ teelebot/handler.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 '''
 @creation date: 2019-8-23
-@last modify: 2020-10-24
+@last modify: 2020-10-29
 '''
 import configparser
 import argparse
@@ -9,7 +9,7 @@ import os
 import sys
 import shutil
 
-__version__ = "1.9.15_dev"
+__version__ = "1.9.16_dev"
 __author__ = "github:plutobell"
 
 parser = argparse.ArgumentParser(description="teelebot console command list")
