b8b81b808f87cf70c4947a8e86f0c3e94f194521
plutobell <53148519+plutobell@users.noreply.github.com>
2020-11-06 00:42:38 +0800

    Updated some default plugins

--- teelebot/plugins/About/About.py
+++ teelebot/plugins/About/About.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 
-
 def About(bot, message):
     chat_id = message["chat"]["id"]
     message_id = message["message_id"]
@@ -12,7 +11,7 @@ def About(bot, message):
         first_btn = g.readline().strip().split(',')
         last_btn = g.readline().strip().split(',')
 
-    if text[1:len(prefix) + 1] == prefix:
+    if text[1:len(prefix)+1] == prefix:
         inlineKeyboard = [
             [
                 {"text": first_btn[0], "url": first_btn[1]},
@@ -23,21 +22,25 @@ def About(bot, message):
             "inline_keyboard": inlineKeyboard
         }
         status = bot.sendChatAction(chat_id, "typing")
-        msg = "此 Bot 基于 <b>teelebot</b> 框架 <b>v" + bot.VERSION + "</b>%0A%0A" + \
-              "<b>teelebot</b> 是基于 Telegram Bot API 的 Bot 框架，具有插件系统，扩展方便。%0A%0A"
+        msg = "此 Bot 基于 <b>teelebot</b> 框架 <b>v" + bot.VERSION + "</b>%0A%0A" +\
+            "<b>teelebot</b> 是基于 Telegram Bot API 的 Bot 框架，具有插件系统，扩展方便。%0A%0A"
 
         req = bot.getUserProfilePhotos(user_id=str(bot_id), limit=1)
-        bot_icon = req.get("photos")[0][0]["file_id"]
-        if type(bot_icon) == str and len(bot_icon) > 50:
-            photo = bot_icon
+        if req.get("photos", "notphotos") != "notphotos":
+            bot_icon = req.get("photos")[0][0]["file_id"]
+            if type(bot_icon) == str and len(bot_icon) > 50:
+                photo = bot_icon
+            else:
+                with open(bot.plugin_dir + "About/icon.png", "rb") as p:
+                    photo = p.read()
         else:
             with open(bot.plugin_dir + "About/icon.png", "rb") as p:
                 photo = p.read()
 
-        status = bot.sendPhoto(chat_id=chat_id, photo=photo, caption=msg, parse_mode="HTML",
-                               reply_to_message_id=message_id, reply_markup=reply_markup)
+        status = bot.sendPhoto(chat_id=chat_id, photo=photo, caption=msg, parse_mode="HTML", reply_to_message_id=message_id, reply_markup=reply_markup)
         bot.message_deletor(15, chat_id, status["message_id"])
     else:
         status = bot.sendChatAction(chat_id, "typing")
         status = bot.sendMessage(chat_id=chat_id, text="指令格式错误，请检查!", parse_mode="HTML", reply_to_message_id=message_id)
         bot.message_deletor(15, chat_id, status["message_id"])
+
