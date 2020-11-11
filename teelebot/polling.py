ab3a7816a619b3f507e9b77d01bd09ad0e162fc9
plutobell <53148519+plutobell@users.noreply.github.com>
2020-11-12 02:58:59 +0800

    Remove dropPendingUpdates

--- teelebot/polling.py
+++ teelebot/polling.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 '''
 @creation date: 2020-6-23
-@last modify: 2020-11-10
+@last modify: 2020-11-12
 '''
 import time
 import sys
@@ -19,15 +19,3 @@ def runUpdates(bot):
                 bot._pluginRun(bot, message)
         except KeyboardInterrupt:
             sys.exit("程序终止")  # 退出存在问题，待修复
-
-
-def dropPendingUpdates(bot):
-    pending_update_count = bot.getWebhookInfo()["pending_update_count"]
-    results = bot.getUpdates()  # 获取消息队列messages
-    messages = bot._washUpdates(results)
-    if messages is None or not messages:
-        pass
-    else:
-        for i, _ in enumerate(messages):  # 获取单条消息记录message
-            if i < pending_update_count:
-                continue
